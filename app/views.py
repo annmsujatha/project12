699c34bb0ba276a74bf795b500837cb89e84a57a
annmsujatha <annmsujatha@gmail.com>
2023-07-28 17:15:03 +0530

    suji

--- app/views.py
+++ app/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def data_render(request):
-   d={'name':'sujji','age':22}
+   d={'name':'sujji','age':3}
    return render(request,'data_render.html',context=d)
+
+
